ca8dcf475e61272455e50b6c0a7c675b08240ff5
Bruno Roemers <bruno@roemers.io>
2021-03-27 15:33:43 +0100

    WIP: Connect to blackbox
    TODO: fix serialization error

--- python/src/app.py
+++ python/src/app.py
@@ -5,25 +5,36 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+import blackbox
+
+
 @app.route('/')
 def hello():
     return 'Hello World'
 
-@app.route('/input', methods=['GET', 'POST'])
-@cross_origin()
-def input():
-    if request.method == 'GET':
-        title = request.args.get('title')
-        director = request.args.get('director')
-        actors = request.args.get('actors')
-        description = request.args.get('description')
-        genres = request.args.get('genres')
-        duration = request.args.get('duration')
-        budget = request.args.get('budget')
-        return request.args
 
-    if request.method == 'POST':
-        return request.json
+@app.route('/prediction')
+@cross_origin()
+def prediction():
+    title = request.args.get('title')
+    director = request.args.get('director')
+    actors = request.args.get('actors')
+    description = request.args.get('description')
+    genres = request.args.get('genres')
+    duration = request.args.get('duration')
+    budget = request.args.get('budget')
+
+    input_parameters = {'Category_to_be_predicted': 'Good', 'Director': 'Zack Snyder', 'Duration': 120,
+                        'Description': 'A very bloody good movie', 'Genre': 'Drama, Horror'}
+    models = blackbox.Classification_Model_Perfected.Train_Models(input_parameters)
+    prediction = blackbox.Classification_Model_Perfected.Prediction_Controller(input_parameters, models)
+
+    # json = {
+    #     prediction: prediction
+    # }
+
+    return prediction
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=7000, debug=True)
